30b0fe3999e2881d273d4b2c2346925f2ab701f3
Sumon <sumoncpi10@gmail.com>
2023-10-19 23:29:57 +0600

    home ok

---
+++ src/components/header/elements/SearchBar.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Select, Button, AutoComplete } from "antd";
+import { useRouter } from "next/router";
+import { SearchOutlined } from "@ant-design/icons";
+
+import { SHOP } from "../../../common/defines";
+import { getProductsByCategory } from "../../../common/shopUtils";
+import {
+  setGlobalCategory,
+  setGlobalSearch,
+} from "../../../redux/actions/globalActions";
+import { setSubCategory } from "../../../redux/actions/shopActions";
+import useDebounce from "../../../common/useDebound";
+
+function SearchBarMobile({ fillData, placeholder }) {
+  const { Option } = Select;
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const [search, setSearch] = useState("");
+  const [showDropdownOptions, setShowDropdownOptions] = useState(false);
+
+  const { category: globalCategory } = useSelector((state) => state.globalReducer);
+  const debouncedValue = useDebounce(search, 300);
+
+  useEffect(() => {
+    dispatch(setGlobalSearch(debouncedValue));
+  }, [debouncedValue]);
+
+  const renderAutoFillItem = () => {
+    const products = getProductsByCategory(fillData, globalCategory);
+    return products.map((item) => ({
+      value: item.name,
+    }));
+  };
+
+  const onSelectCategory = (value) => {
+    dispatch(setGlobalCategory(value));
+    dispatch(setSubCategory(""));
+  };
+
+  const openDropdownOptions = (value) => {
+    setShowDropdownOptions(true);
+    setSearch(value);
+  };
+
+  const closeDropdownOptions = () => {
+    setShowDropdownOptions(false);
+  };
+
+  const onSelectOption = (value) => {
+    setSearch(value);
+    closeDropdownOptions();
+  };
+
+  const onSearch = () => {
+    if (!search || search === "") {
+      router.push("/");
+    } else {
+      router.push({
+        pathname: "/",
+        query: { q: search },
+      });
+    }
+  };
+
+  return (
+    <div className="menu-search">
+      <div className="menu-search__form">
+        <Select
+          className="menu-search__form-select"
+          defaultValue={globalCategory}
+          style={{ width: 150, marginBottom: 8 }}
+          onChange={onSelectCategory}
+          value={globalCategory}
+        >
+          {SHOP.category.map((item, index) => (
+            <Option key={index} value={item.name}>
+              {item.name}
+            </Option>
+          ))}
+        </Select>
+        <div className="menu-search__form-input">
+          <AutoComplete
+            allowClear
+            backfill={true}
+            open={showDropdownOptions}
+            onSearch={openDropdownOptions}
+            onBlur={closeDropdownOptions}
+            onSelect={onSelectOption}
+            options={renderAutoFillItem()}
+            placeholder={placeholder}
+            filterOption={(inputValue, option) =>
+              option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+            }
+          />
+          <Button onClick={onSearch}>
+            <SearchOutlined />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default React.memo(SearchBarMobile);
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
+
+import { persistor, store } from "../redux/store";
+// import "@/styles/antd.less";
+import "@/styles/less.css";
+import "../styles/styles.scss";
+import Loading from "../components/other/Loading";
+import withReduxStore from "../common/withReduxStore";
+
+const App = ({ Component, pageProps, reduxStore }) => {
+  return (
+    <Provider store={reduxStore}>
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  );
+};
+
+export default withReduxStore(App);
