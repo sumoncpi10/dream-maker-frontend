aaf8f622800ee4867dde3772ff988227ced134d0
Sumon <sumoncpi10@gmail.com>
2023-10-22 00:04:10 +0600

    asgfds

--- src/common/WithHeaderScroll.js
+++ src/common/WithHeaderScroll.js
@@ -1,28 +1,30 @@
 import React, { useState, useEffect, useRef } from "react";
 
 export default function WithHeaderScroll(WrappedComponent) {
-  return function (props) {
+  function HeaderScroll(props) {
     const headerRef = useRef(null);
     const [scroll, setScroll] = useState(0);
-    const [isHeaderFixed, setIsHeaderFixed] = useState(0);
+    const [isHeaderFixed, setIsHeaderFixed] = useState(false);
+
     useEffect(() => {
+      function handleScroll() {
+        setScroll(window.scrollY);
+      }
+
       window.addEventListener("scroll", handleScroll);
+
       return () => {
         window.removeEventListener("scroll", handleScroll);
       };
     }, []);
 
     useEffect(() => {
-      if (scroll >= headerRef?.current?.offsetHeight) {
+      if (scroll >= headerRef.current.offsetHeight) {
         setIsHeaderFixed(true);
       } else {
         setIsHeaderFixed(false);
       }
     }, [scroll]);
-
-    function handleScroll() {
-      setScroll(window?.scrollY);
-    }
 
     return (
       <div
@@ -32,5 +34,15 @@ export default function WithHeaderScroll(WrappedComponent) {
         <WrappedComponent {...props} />
       </div>
     );
-  };
+  }
+
+  // Set the display name for the component
+  HeaderScroll.displayName = `WithHeaderScroll(${getDisplayName(WrappedComponent)})`;
+
+  return HeaderScroll;
+}
+
+// Helper function to get the display name of a component
+function getDisplayName(WrappedComponent) {
+  return WrappedComponent.displayName || WrappedComponent.name || "Component";
 }
