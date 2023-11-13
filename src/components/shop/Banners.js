8378b3ef605b60af61162bbf6b8fdb7c6a50328d
Sumon <sumoncpi10@gmail.com>
2023-11-14 00:09:43 +0600

    sgb

--- src/components/shop/Banners.js
+++ src/components/shop/Banners.js
@@ -5,6 +5,7 @@ import Container from "../other/Container";
 import ScrolBaner from "./ScrolBaner";
 import ShopSidebar from "./ShopSidebar";
 import TypeSidebar from "./TypeSidebar";
+import Image from "next/image";
 
 const contentStyle = {
   height: '390px',
@@ -20,22 +21,21 @@ export default function Banners({ containerType, itemType }) {
       <Container type={containerType}>
         <Row gutter={30}>
           {/* Shop Sidebar */}
-          <Col className="gutter-row" xs={24} sm={6} lg={6} >
+          <Col xs={24} sm={6} lg={6}>
             <TypeSidebar itemType={itemType} />
           </Col>
 
           {/* Shop Content with Carousel */}
-          <Col className="gutter-row" xs={24} sm={18} lg={18}>
+          <Col xs={24} sm={18} lg={18}>
             <Carousel autoplay>
               {Array.from({ length: 3 }, (item, index) => (
-                <Col key={index} className="gutter-row" span={24} sm={8} >
+                <Col key={index} span={24} sm={8}>
                   <Link href={process.env.PUBLIC_URL + "#"}>
                     <div className="banner-item" style={contentStyle}>
-                      <img
-                        src={
-                          // process.env.PUBLIC_URL +
-                          `/assets/images/banners/${index + 1}.jpg`
-                        }
+                      <Image
+                        width={850}
+                        height={390}
+                        src={`/assets/images/banners/${index + 1}.jpg`}
                         alt="banner"
                       />
                     </div>
